f88f14671d5c87684de10c29f326beea2f55a1c7
zeftambornino@gmail.com <zeftambornino@gmail.com>
2019-09-11 18:11:45 -0500

    installed packages

---
+++ liri.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+var keys = require("./keys.js");
+var spotify = new Spotify(keys.spotify);
+
+// `concert-this`
+// Name of the venue
+// Venue location
+// Date of the Event(use moment to format this as "MM/DD/YYYY")
+
+"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+// `spotify-this-song`
+
+// `movie-this`
+
+// `do-what-it-says`
