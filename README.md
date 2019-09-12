b68b9289831fb3bf690d4a9f20383f3a39e23680
zeftambornino@gmail.com <zeftambornino@gmail.com>
2019-09-12 17:58:02 -0500

    Added Bandsintown & OMBD

--- README.md
+++ README.md
@@ -1,11 +1,59 @@
 # liri-node-app
-LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies. This app is designed to pull the most important bits of information from several APIs and log it into the console using Node.JS. This is for self-educational purposes and also serving a useful function.
+## Developed by Zeph Tambornino
+LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies. This app is designed to pull the most important bits of information from several APIs and log it into the console using Node.JS. This is for self-educational purposes and also serving a practical function.
+
+### The Overview
+The APIs utilized in this application are from Bands in Town, Spotify, and OMDB. Their usage is to give the following information for each search:
+
+1. `Bands in Town`
+     * Name of the venue
+
+     * Venue location
+
+     * `Date` of the Event (uses `moment.js` to format as "MM/DD YYYY")
 
 
+2. `Spotify`
+     * Artist(s)
 
-2. Give a high-level overview of how the app is organized
+     * The song's name
+
+     * A preview link of the song from Spotify
+
+     * The album that the song is from
+
+
+3. `OMDB`
+     * Title of the movie.
+
+     * Year the movie came out.
+
+     * IMDB Rating of the movie.
+
+     * Rotten Tomatoes Rating of the movie.
+
+     * Country where the movie was produced.
+
+     * Language of the movie.
+
+     * Plot of the movie.
+
+     * Actors in the movie.
+
+
+4. `Output Log`
+     * In addition to logging the data to your terminal/bash window, outputs the data to a .txt file called `log.txt`.
+
+### Instructions
 3. Give start-to-finish instructions on how to run the app
-4. Include screenshots, gifs or videos of the app functioning
-5. Contain a link to a deployed version of the app
-6. Clearly list the technologies used in the app
-7. State your role in the app development
+
+### Screenshots & Gifs
+
+### Technologies Used
+*
+     * `Node.js`
+     * `dotenv` package
+     * `axios` package
+     * `moment.js` package
+     * `Bandsintown`, `Spotify`, & `OMDB` APIs/Packages
+
--- liri.js
+++ liri.js
@@ -1,15 +1,95 @@
 require("dotenv").config();
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+const keys = require("./keys.js");
+//var spotify = new Spotify(keys.spotify);
+const axios = require("axios");
+const moment = require("moment");
+// creating a table of contents
+// creating const ConcertThis
+const ConcertThis = function () {
+    //using our process.argv to search our Bandsintown API
+    this.artistSearch = function (artist) {
+        //the Bandsintown API url with our added artist input
+        const queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+        // fullfils a promise to grab our url and grab the JSON response
+        axios.get(queryUrl).then(function (response) {
+            // displays a message if the band is currently not touring
+            if (response.data.length < 1) {
+                console.log("Band currently has no scheduled concerts")
+                return
+            }
+            // creating several const to hold our response data
+            const venue = response.data[0].venue.name;
+            //formatting the time of the event using moment.js
+            const time = moment(response.data[0].datetime).format("MM/DD/YYYY");
+            const city = response.data[0].venue.city;
+            const country = response.data[0].venue.country;
+            // logging our information with special formating
+            // allows for readability
+            console.log("------------",
+                "\nVenue: " + venue,
+                "\n\nLocation: " + city + ", " + country,
+                "\n\nTime: " + time,
+                "\n------------")
+        })
+    }
+};
 
-// `concert-this`
-// Name of the venue
-// Venue location
-// Date of the Event(use moment to format this as "MM/DD/YYYY")
+// if the user types concert-this they will use the ConcertThis function
+// creating const MovieThis
+const MovieThis = function () {
+    //using our process.argv to search our OMDB API
+    this.movieSearch = function (title) {
+        //the OMDB API url with our added title input
+        const queryUrl = "http://www.omdbapi.com/?apikey=trilogy&t=" + title
+        // fullfils a promise to grab our url and grab the JSON response
+        axios.get(queryUrl).then(function (response) {
+            // creating several const to hold our response data
+            const Title = response.data.Title;
+            const Released = response.data.Released;
+            const imdbRatings = response.data.Ratings[0].Value;
+            const IMDB = response.data.Ratings[0].Source;
+            const rottenRatings = response.data.Ratings[1].Value;
+            const rottenTomatoes = response.data.Ratings[1].Source;
+            const Country = response.data.Country;
+            const Language = response.data.Language;
+            const Plot = response.data.Plot;
+            const Actors = response.data.Actors;
+            console.log("------------",
+                "\nTitle: " + Title,
+                "\n\nRelease: " + Released,
+                "\n\nRatings: " + imdbRatings + ", " + IMDB,
+                "\n\nRatings: " + rottenRatings + ", " + rottenTomatoes,
+                "\n\nCountry: " + Country,
+                "\n\nLanguage: " + Language,
+                "\n\nPlot: " + Plot,
+                "\n\nStarring: " + Actors,
+                "\n------------")
+        })
+    }
+};
+const showSearch = new ConcertThis();
+const filmSearch = new MovieThis();
+const search = process.argv[2];
+let term = process.argv[3];
 
-"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
-// `spotify-this-song`
+function userSearch() {
+    if (!search) {
+        return console.log("Try 'Help'")
+    }
+    // if the user types concert-this they will use the ConcertThis function
+    else if (search === "movie-this") {
+        if (!term) {
+            term = "Mr. Nobody"
+        }
+        filmSearch.movieSearch(term);
 
-// `movie-this`
-
-// `do-what-it-says`
+} else if (search === "concert-this") {
+    if (!term) {
+        console.log("Please provide Band/Artist name")
+        return
+    }
+    showSearch.artistSearch(term);
+}
+}
+userSearch();
+// `do-what-it-says
